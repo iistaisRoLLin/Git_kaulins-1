c26427d06e83b01586ad154f741185fcbf531446
Ravalds <kristaps.ravalds@lvt.lv>
2022-01-18 10:17:43 +0200

    Pamainīta rindiņu secība

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,9 +2,8 @@ import java.util.Random;
 
 public class Kaulins {
 	public static void main(String[] args) {
-		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
+		int skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita :"+skaitlis);
 	}
 }
