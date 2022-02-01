75af0839d1af71c7e4ff2cd7735cfada3dc3976c
Cernisevs <kristers.cernisevs@gmail.com>
2022-02-01 11:37:42 +0200

    Manas veiktas izmainas Kristapa programma

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -13,12 +13,21 @@ public class Kaulins {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int reizes;
-		do {
-			System.out.println("Cik reizes mest kauliņu?");
-			reizes = scan.nextInt();
-		}while(reizes<1);
-		mestKaulinu(reizes);	
+		int reizes, izvele;
+		do{
+			System.out.println("1-Mest | 2. Apturet");
+			izvele = scan.nextInt();
+			switch(izvele){
+			case 1:
+				do{
+					System.out.println("Cik reizes mest kauliņu?");
+					reizes = scan.nextInt();
+				}while(reizes<1);
+				mestKaulinu(reizes);
+				break;
+			}
+		}while(izvele!=2);
+			
 		scan.close();
 	}
 }
